85f950176a568356a5e055606b78d45c33c3b6b1
sludwikowski <ludwikowski.szym1993@gmail.com>
2023-11-11 20:41:40 +0100

    feat(build)

---
+++ components/awards/Awards.tsx
@@ -0,0 +1,15 @@
+import AwardsItem from './AwardsItem'
+import { getAwardsData } from '../../data/awards'
+import React from 'react'
+
+function Awards() {
+  return (
+    <div className="awards-inner mt-30">
+      {getAwardsData().map((item, index) => (
+        <AwardsItem data={item} key={index} />
+      ))}
+    </div>
+  )
+}
+
+export default Awards
---
+++ components/box-information/BoxRight.tsx
@@ -0,0 +1,19 @@
+// @ts-nocheck
+import ParallaxImage, { ParallaxImageProps } from '../Image/ParallaxImage'
+import { dsnCN } from '../../hooks/helper'
+import React from 'react'
+
+interface BoxRightProps extends ParallaxImageProps {}
+
+function BoxRight({ className, children, ...restProps }: BoxRightProps) {
+  return (
+    <div className="box-seat box-seat-full">
+      <div className="box-im h-100">
+        <ParallaxImage height={'100%'} overlay={2} {...restProps} />
+      </div>
+      <div className={dsnCN('pro-text ml-auto box-padding', className)}>{children}</div>
+    </div>
+  )
+}
+
+export default BoxRight
